bb733cfe7fa149b114a8739bbf9b70d3d9cceaac
sjproctor <sproctor950@gmail.com>
2020-10-06 17:58:02 -0700

    adds delete and update functionality

--- rails/full-stack-rails/app/controllers/herb_controller.rb
+++ rails/full-stack-rails/app/controllers/herb_controller.rb
@@ -1,23 +1,23 @@
 class HerbController < ApplicationController
 
-  # index
+  # index - see all
   def index
     @herbs = Herb.all
   end
 
-  #show
+  # show - see one by id
   def show
     @herb = Herb.find(params[:id])
   end
 
+  # new - form
   def new
+    @herb = Herb.new
   end
 
+  # post/create - submit a form
   def create
-    @herb = Herb.create(
-      name: params[:name],
-      is_watered: params[:is_watered]
-    )
+    @herb = Herb.create(herb_params)
     if @herb.valid?
       redirect_to herbs_path
     else
@@ -25,4 +25,36 @@ class HerbController < ApplicationController
     end
   end
 
+  # delete
+  def destroy
+    @herb = Herb.find(params[:id])
+    if @herb.destroy
+      redirect_to herbs_path
+    else
+      redirect_to herb_path(@herb)
+    end
+  end
+
+  # edit - form
+  def edit
+    @herb = Herb.find(params[:id])
+  end
+
+  # update
+  def update
+    @herb = Herb.find(params[:id])
+    @herb.update(
+      name: params[:name],
+      is_watered: params[:is_watered]
+    )
+    if @herb.valid?
+      redirect_to herb_path(@herb)
+    else
+      redirect_to herbs_path
+    end
+  end
+
+  def herb_params
+    params.require(:herb).permit(:name, :is_watered)
+  end
 end
--- rails/full-stack-rails/config/routes.rb
+++ rails/full-stack-rails/config/routes.rb
@@ -1,6 +1,11 @@
+# ROUTES
+
 Rails.application.routes.draw do
   get 'herbs' => 'herb#index', as: 'herbs'
   get 'herbs/new' => 'herb#new', as: 'new_herb'
-  get 'herbs/:id' => 'herb#show', as: 'herb'
   post 'herbs' => 'herb#create'
+  get 'herbs/:id' => 'herb#show', as: 'herb'
+  get 'herbs/:id/edit' => 'herb#edit', as: 'edit_herb'
+  patch 'herbs/:id' => 'herb#update'
+  delete 'herbs/:id' => 'herb#destroy', as: 'delete_herb'
 end
